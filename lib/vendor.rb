095ee3c31c989a952b55f424a8a61e525a03792a
Young Koh <youngheon6@gmail.com>
2023-03-07 09:27:53 -0700

    feat: Define and Test #check_stock

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -3,6 +3,18 @@ class Vendor
   
   def initialize(name)
     @name = name
-    @inventory = {}
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    if @inventory.include?(item)
+      @inventory[item]
+    else
+      0
+    end
+  end
+
+  def stock(item,quantitity)
+    @inventory[item] += quantity
   end
 end
--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -4,6 +4,8 @@ require './lib/vendor'
 describe Vendor do
   before(:each) do
     @vendor = Vendor.new("Rocky Mountain Fresh")
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
   end
 
   describe "#initialize" do
@@ -11,6 +13,25 @@ describe Vendor do
       expect(@vendor).to be_instance_of(Vendor)
       expect(@vendor.name).to eq('Rocky Mountain Fresh')
       expect(@vendor.inventory).to eq({})
+    end
+  end
+
+  describe "#check_stock" do
+    it 'returns 0 if item is not in stock' do
+      expect(@vendor.check_stock(@item1)).to eq(0)
+    end
+
+    it 'checks given item stock' do
+    end
+  end
+
+  describe "#stock" do
+    it 'stocks the given item at given quantitity' do
+      expect(@vendor.inventory).to eq({})
+
+      @vendor.stock(@item1, 30)
+
+      expect(@=)
     end
   end
 end
